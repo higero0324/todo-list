f003f922bc7c0b273b51c2a60f8ae670670027d2
higero0324 <180026540+higero0324@users.noreply.github.com>
2025-11-13 17:16:29 +0900

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repositoryName = 'todo-list'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-list/',
+  base: process.env.NODE_ENV === 'production' ? `/${repositoryName}/` : '/',
 })
